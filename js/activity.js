9fadf7e936cc4319965739a3bcf937bca33daca4
Zeel Soni <zeeljsoni@gmail.com>
2026-08-15 14:38:52 -0700

    Added responsive form and table

--- js/activity.js
+++ js/activity.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+    var selectedActivities = [];
+    var $modalList = $('#modalActivityList');
+    var $result = $('#result');
+
     $('#activities table tbody tr').each(function() {
         $(this).find('td').not(':first').each(function() {
             var cellText = $(this).text().trim();
@@ -11,30 +15,48 @@ $(document).ready(function() {
         });
     });
 
+    function updateSelectedList() {
+        if (selectedActivities.length === 0) {
+            $result.html('<h4>My selected activities to enquire:</h4>');
+            $modalList.text('No activity selected.');
+            return;
+        }
+
+        var listHtml = selectedActivities.map(function(item) {
+            return '<p>' + item + '</p>';
+        }).join('');
+
+        $result.html('<h4>My selected activities to enquire:</h4>' + listHtml);
+        $modalList.html('<ul class="mb-0">' + selectedActivities.map(function(item) {
+            return '<li>' + item + '</li>';
+        }).join('') + '</ul>');
+    }
+
     $('.activity-cell.selectable').on('click', function() {
-        $(this).toggleClass('selected');
+        var activity = $(this).closest('tr').find('td:first').text().trim();
+        var cliffName = $('#activities table thead th').eq($(this).index()).text().trim();
+        var itemText = activity + ' - ' + cliffName;
+        var itemIndex = selectedActivities.indexOf(itemText);
+
+        if ($(this).hasClass('selected')) {
+            selectedActivities.splice(itemIndex, 1);
+            $(this).removeClass('selected');
+        } else {
+            selectedActivities.push(itemText);
+            $(this).addClass('selected');
+        }
+
+        updateSelectedList();
     });
 
-    $("td").click(function () { //user select a table data cell
-        var content = $(this).text(); //get content of cell
-        var cliffName = $('#activities table thead th').eq($(this).index()).text(); //get the matching cliff site name
-        var displayText = content + ' - ' + cliffName; //combine activity and cliff site
-        
-        if (content != "Not Available") { //check if content does not contain a particular string
-            $(this).toggleClass("tdhighlight"); //add or remove class when cell is selected
-            
-            if ($(this).hasClass("tdhighlight")) { //check if selected cell has class
-                $('#displaySelected').css("visibility", "visible"); //make display box visible
-                $('#displaySelected').css("margin-top", "2em"); //add spaces above display box
-                $('#result').append("<p>"+displayText+"</p>"); //add child element with contents of cell
-            } else { //if selected cell don't have class
-                $('#result p:contains('+displayText+')').remove(); //remove child element
-                
-                if ($('#result').has('p').length == false) { //check if there are any child elements within parent
-                    $('#displaySelected').css("visibility", "hidden"); //make display box hidden
-                    $('#displaySelected').css("margin-top", "0"); //remove spaces above display box
-                }
-            }
+    $('#sendinfo button').on('click', function() {
+        if (selectedActivities.length === 0) {
+            $modalList.text('Please select at least one activity from the table.');
+            $('#activityModal').modal('show');
+            return;
         }
+
+        updateSelectedList();
+        $('#activityModal').modal('show');
     });
 });
